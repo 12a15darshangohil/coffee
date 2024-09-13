c53bfd9121e8df11072803606861ba37e093e6a0
12a15darshangohil <12a15darshangohil@gmail.com>
2024-09-13 23:08:35 +0530

    commit

--- frontend/src/Pages/Coffe At Home.js
+++ frontend/src/Pages/Coffe At Home.js
@@ -1,11 +1,18 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 const CoffeeAtHome = () =>{
-    const data=[1,2,3,4,5]
-    useEffect(()=>{
-        // fetch("http://127.0.0.1:8000/api/items/")
-        // .then( res => res.json())
-        // .then(data =>{console.log(data)})
-    })
+    const [CoffeeAtHome, setCoffeeAtHome] = useState([]);
+    useEffect(() => {
+        // Step 2: Fetch data from API
+        fetch("http://127.0.0.1:8000/api/coffee-at-home/")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setCoffeeAtHome(data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
     return(
         <>
             <div className="sm:px-0 md:w-4/5 m-auto ">
@@ -14,12 +21,12 @@ const CoffeeAtHome = () =>{
             </div>
         <div className=" sm:px-0 md:w-4/5 m-auto flex flex-wrap items-center mb-24 ">
             {
-                data.map((item)=>{
+                CoffeeAtHome.map((CoffeeAtHome,index)=>{
                     return(
                     <>
-                    <div className='flex w-[420px]  px-4 bg-[#F9F9F9] py-5 rounded-[8px] item_container mr-[28px] my-[15px] scaleA'>
+                    <div key={index} className='flex w-[420px]  px-4 bg-[#F9F9F9] py-5 rounded-[8px] item_container mr-[28px] my-[15px] scaleA'>
                     <div>
-                        <img src='https://starbucksstatic.cognizantorderserv.com/Items/Small/M107781.jpg' className='rounded-full ' style={{ maxWidth: "99px", maxHeight: "99px" }} />
+                        <img src={CoffeeAtHome.img} className='rounded-full ' style={{ maxWidth: "99px", maxHeight: "99px" }} />
                     </div>
                     <div className='flex flex-col '>
                         <div className='px-4 mb-[5px]' style={{ minHeight: "150px" }}>
@@ -27,14 +34,14 @@ const CoffeeAtHome = () =>{
                                 <img src="https://www.starbucks.in/assets/icon/veg.svg" alt="veg-item" style={{ width: '16px', height: '16px' }} />
                             </div>
                             <div className='mb-1.5 text-[18px] font-medium'>
-                                Iced Stardust Macchiato
+                                {CoffeeAtHome.title}
                             </div>
-                            <div className='mb-2 text-[13px] font-normal text-[#212529]'>
-                                Super smooth and creamy vanilla half and half crowned with s...
+                            <div className='mb-2 text-[13px] font-normal text-[#212529ad] FontTitle'>
+                                {CoffeeAtHome.text}
                             </div>
                         </div>
                         <div className='flex flex-row justify-between px-3'>
-                            <div className='text-[20px] font-serif font-bold'> $ 341</div>
+                            <div className='text-[20px] font-serif'> &#8377; {CoffeeAtHome.price} </div>
                             <div className='px-6 py-2 bg-[#979797] rounded-[30px] text-[14px] font-bold text-[#C6C6C6] Add_item'>Add Item</div>
                         </div>
                     </div>
--- frontend/src/Pages/ReadyToeat.js
+++ frontend/src/Pages/ReadyToeat.js
@@ -1,15 +1,13 @@
 import { useEffect,useState } from "react";
-import { useLocation } from "react-router-dom";
-const Food = () =>{
-    const Location=useLocation();
-    const [Food, setFood] = useState([]);
+const ReadyToEat = () =>{
+    const [CoffeeAtHome, setCoffeeAtHome] = useState([]);
     useEffect(() => {
         // Step 2: Fetch data from API
-        fetch("http://127.0.0.1:8000/api/food/")
+        fetch("http://127.0.0.1:8000/api/ready-to-eat/")
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                setFood(data);
+                setCoffeeAtHome(data);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
@@ -18,17 +16,17 @@ const Food = () =>{
     return(
         <>
             <div className="sm:px-0 md:w-4/5 m-auto ">
-                <p className="font-bold text-[#000000c4] mt-20 text-[18px] ">Sandwiches & Wraps</p>
-                <p className="text-[#000000af] mt-2 mb-7">Signature breads made with fresh ingredients and in-house sauces.</p>
+                <p className="font-bold text-[#000000c4] mt-20 text-[18px] ">Quick Bites</p>
+                <p className="text-[#000000af] mt-2 mb-7">Starbucks snacks available on the go.</p>
             </div>
         <div className=" sm:px-0 md:w-4/5 m-auto flex flex-wrap items-center mb-24 ">
             {
-                Food.map((Food,index)=>{
+                CoffeeAtHome.map((CoffeeAtHome,index)=>{
                     return(
                     <>
-                    <div  key={index} className='flex w-[420px]  px-4 bg-[#F9F9F9] py-5 rounded-[8px] item_container mr-[28px] my-[15px] scaleA'>
+                    <div key={index} className='flex w-[420px]  px-4 bg-[#F9F9F9] py-5 rounded-[8px] item_container mr-[28px] my-[15px] scaleA'>
                     <div>
-                        <img src={Food.img} className='rounded-full ' style={{ maxWidth: "99px", maxHeight: "99px" }} />
+                        <img src={CoffeeAtHome.img} className='rounded-full ' style={{ maxWidth: "99px", maxHeight: "99px" }} />
                     </div>
                     <div className='flex flex-col '>
                         <div className='px-4 mb-[5px]' style={{ minHeight: "150px" }}>
@@ -36,14 +34,14 @@ const Food = () =>{
                                 <img src="https://www.starbucks.in/assets/icon/veg.svg" alt="veg-item" style={{ width: '16px', height: '16px' }} />
                             </div>
                             <div className='mb-1.5 text-[18px] font-medium'>
-                               { Food.title ||'Iced Stardust Macchiato'}
+                                {CoffeeAtHome.title}
                             </div>
-                            <div className='mb-2 text-[13px] font-normal text-[#2125297d] FontTitle '>
-                                {Food.text}
+                            <div className='mb-2 text-[13px] font-normal text-[#212529ad] FontTitle'>
+                                {CoffeeAtHome.text}
                             </div>
                         </div>
                         <div className='flex flex-row justify-between px-3'>
-                            <div className='text-[20px] font-serif '> &#8377; {Food.price}</div>
+                            <div className='text-[20px] font-serif'> &#8377; {CoffeeAtHome.price} </div>
                             <div className='px-6 py-2 bg-[#979797] rounded-[30px] text-[14px] font-bold text-[#C6C6C6] Add_item'>Add Item</div>
                         </div>
                     </div>
@@ -56,4 +54,4 @@ const Food = () =>{
         </>
     )
 }
-export default Food;
+export default ReadyToEat;
