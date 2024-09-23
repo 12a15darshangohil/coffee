f73f01ae980064fa1d39a90c5b403c3306c4f4e3
darshangohil46 <darshangohil2005@gmail.com>
2024-09-23 19:32:28 +0530

    suggest by rk

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -7,6 +7,8 @@ const Login = ({ skip }) => {
     const navigate = useNavigate(); // useNavigate for navigation
 
     let [login, setLoginn] = useState(true)
+
+    const [authDone, setAuthDone] = useState(false)
     const [userData, setUserData] = useState(null);
 
     useEffect(() => {
@@ -38,7 +40,7 @@ const Login = ({ skip }) => {
                 if (response.ok) {
                     alert('Login Successfully');
                     window.localStorage.setItem('loggedIn', true);
-                    window.location.reload()
+                    // window.location.reload()
 
                     // Fetch authenticated user data from another API
                     const userResponse = await fetch('http://localhost:8000/api/user-auth/', {
@@ -46,11 +48,11 @@ const Login = ({ skip }) => {
                         credentials: 'include',
                     });
 
-
                     if (userResponse.ok) {
                         const userData = await userResponse.json();
                         console.log("userdata:", userData);
                         setUserData(userData); // Store user data in state
+                        setAuthDone(true); // Set authentication done
                         window.localStorage.setItem('loggedIn', true);
                     } else {
                         alert('Failed to fetch user data.');
@@ -94,69 +96,75 @@ const Login = ({ skip }) => {
         }
     }
 
+    // Navigate to user dashboard
+    const goToDashboard = () => {
+        navigate('/userdashboard');
+    };
+
 
     return (
         <>
-            {login ?
-                <div className='auth-container' id='auth-container'>
-                    <div className='auth-box scaleA'>
-                        <div className='text-[#00754a] font-semibold text-[14px]' style={{ textAlign: "end" }} onClick={() => { skip(false) }}>SKIP</div>
-                        <div className='auth-header'>Login</div>
-                        <form onSubmit={loginn} id="loginn">
-                            <div>
-                                <label className='label_login'>USERNAME</label>
-                                <br />
-                                <input type='text' placeholder='Enter Username *' className='in_login pb-1' id="lusername" />
-                            </div>
-                            <div className='pt-4'>
-                                <label className='label_login'>PASSWORD</label>
-                                <br />
-                                <input type='password' placeholder='Enter Password *' className='in_login pb-1' id="lpass" />
-                            </div>
+            <>
+                {login ?
+                    <div className='auth-container' id='auth-container'>
+                        <div className='auth-box scaleA'>
+                            <div className='text-[#00754a] font-semibold text-[14px]' style={{ textAlign: "end" }} onClick={() => { skip(false) }}>SKIP</div>
+                            <div className='auth-header'>Login</div>
+                            <form onSubmit={loginn} id="loginn">
+                                <div>
+                                    <label className='label_login'>USERNAME</label>
+                                    <br />
+                                    <input type='text' placeholder='Enter Username *' className='in_login pb-1' id="lusername" />
+                                </div>
+                                <div className='pt-4'>
+                                    <label className='label_login'>PASSWORD</label>
+                                    <br />
+                                    <input type='password' placeholder='Enter Password *' className='in_login pb-1' id="lpass" />
+                                </div>
 
-                            <div className='pt-4 text-[13px] font-semibold text-[#0000008b]'>
-                                Don't have an account? <span className='text-[#00754a] underline' onClick={() => {
-                                    document.getElementById('loginn').reset();
-                                    setLoginn(false)
-                                }}> SignUp</span></div>
+                                <div className='pt-4 text-[13px] font-semibold text-[#0000008b]'>
+                                    Don't have an account? <span className='text-[#00754a] underline' onClick={() => {
+                                        document.getElementById('loginn').reset();
+                                        setLoginn(false)
+                                    }}> SignUp</span></div>
 
-                            <div className='pt-9 pb-2' style={{ textAlign: "center", alignItems: "center" }}>
-                                <button type="submit" className='login_b'>Login</button>
-                            </div>
-                        </form>
+                                <div className='pt-9 pb-2' style={{ textAlign: "center", alignItems: "center" }}>
+                                    <button type="submit" className='login_b'>Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-                :
-                <div className='auth-container'>
-                    <div className='auth-box'>
-                        <div className='text-[#00754a] font-semibold text-[14px]' style={{ textAlign: "end" }} onClick={() => { skip(false) }}>SKIP</div>
-                        <div className='auth-header'>SignUp</div>
-                        <form onSubmit={SignUpp}>
-                            <div>
-                                <label className='label_login'>USERNAME</label>
-                                <br />
-                                <input type='text' placeholder='Enter Username *' className='in_login pb-1' id="Susername" />
-                            </div>
-                            <div className='pt-4'>
-                                <label className='label_login'>CREATE PASSWORD</label>
-                                <br />
-                                <input type='password' placeholder='Enter Mobilenumber *' className='in_login pb-1' id="mobilenumber" />
-                            </div>
-                            <div className='pt-4'>
-                                <label className='label_login'>CONFIRM PASSWORD</label>
-                                <br />
-                                <input type='password' placeholder='Create Password *' className='in_login pb-1' id="SCpassword" />
-                            </div>
+                    :
+                    <div className='auth-container'>
+                        <div className='auth-box'>
+                            <div className='text-[#00754a] font-semibold text-[14px]' style={{ textAlign: "end" }} onClick={() => { skip(false) }}>SKIP</div>
+                            <div className='auth-header'>SignUp</div>
+                            <form onSubmit={SignUpp}>
+                                <div>
+                                    <label className='label_login'>USERNAME</label>
+                                    <br />
+                                    <input type='text' placeholder='Enter Username *' className='in_login pb-1' id="Susername" />
+                                </div>
+                                <div className='pt-4'>
+                                    <label className='label_login'>CREATE PASSWORD</label>
+                                    <br />
+                                    <input type='password' placeholder='Enter Mobilenumber *' className='in_login pb-1' id="mobilenumber" />
+                                </div>
+                                <div className='pt-4'>
+                                    <label className='label_login'>CONFIRM PASSWORD</label>
+                                    <br />
+                                    <input type='password' placeholder='Create Password *' className='in_login pb-1' id="SCpassword" />
+                                </div>
 
-                            <div className='pt-9 pb-2' style={{ textAlign: "center", alignItems: "center" }}>
-                                <button type="submit" className='login_b'>Login</button>
-                            </div>
-                        </form>
+                                <div className='pt-9 pb-2' style={{ textAlign: "center", alignItems: "center" }}>
+                                    <button type="submit" className='login_b'>Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            }
+                }
+            </>
         </>
-
     );
 };
 export default Login;
